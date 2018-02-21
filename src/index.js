c3f682303970d9d6bb93561863a3f1dfcea91e76
Artsiom Lipin <lipinartsiom@gmail.com>
2018-02-21 22:20:56 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  let inputData = equation.split(" ");
+  let a = parseInt(inputData[0]);
+  let b = parseInt(inputData[3] + inputData[4]);
+  let c = parseInt(inputData[7] + inputData[8]);
+  let discriminant = (b * b) - (4 * a * c);
+  let x1 = Math.round( (-b + Math.sqrt(discriminant)) / (2 * a) );
+  let x2 = Math.round( (-b - Math.sqrt(discriminant)) / (2 * a) );
+  let solution = [];
+  solution.push(x1, x2);
+  solution.sort(function(a, b){ return a - b; });
+  return solution;
+};
